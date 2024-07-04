da4a01c37e4d5552c8589adb6b6903b481c530d7
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-07-05 01:26:10 +0800

    Implemented Interactive Sign
    - Implemented interactive sign

---
+++ TheLastKnight/Assets/Scripts/EnvObjs/InteractiveSign.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using TMPro;
+
+
+public class InteractiveSign : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueBox; 
+    [SerializeField] TextMeshProUGUI dialogueText; 
+    [SerializeField] string dialogue; 
+    [SerializeField] bool playerInRange; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogueBox.activeInHierarchy)
+            {
+                dialogueBox.SetActive(false); 
+            } else {
+                dialogueBox.SetActive(true); 
+                dialogueText.text = dialogue; 
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player")) {
+            playerInRange = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player")) {
+            playerInRange = false;  
+            dialogueBox.SetActive(false); 
+        }
+    }
+
+}
