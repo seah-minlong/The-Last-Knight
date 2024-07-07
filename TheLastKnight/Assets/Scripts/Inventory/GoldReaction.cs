93b8b78472e3f5e53b6f1b413336459c646d5d52
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-07-07 22:06:51 +0800

    Updates to Gold Inventory system
    - Minor bug fixes

--- TheLastKnight/Assets/Scripts/Inventory/GoldReaction.cs
+++ TheLastKnight/Assets/Scripts/Inventory/GoldReaction.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldReaction : MonoBehaviour
-{
-    public FloatValue heartContainers; 
-    public FloatValue playerHealth; 
-    public MySignal useGoldSignal; 
-    //public HeartManager heartManager; 
+// public class GoldReaction : MonoBehaviour
+// {
+//     public FloatValue heartContainers; 
+//     public FloatValue playerHealth; 
+//     public MySignal useGoldSignal; 
 
-    public void UseGold(int amountToIncrease)
-    {
-        Debug.Log("use gold called"); 
-        //signal calls IncreaseHeartContainers() 
-        useGoldSignal.Raise(); 
-    }
-}
+//     // public void UseGold(int amountToIncrease)
+//     // {
+//     //     Debug.Log("use gold called"); 
+//     //     //signal calls IncreaseHeartContainers() 
+//     //     useGoldSignal.Raise(); 
+//     // }
+// }
